e6e55b71ce7bcd26d3b7972c04930dba7fcc9268
ingabire-byte <ingabirejoy25@gmail.com>
2023-11-13 07:47:30 -0800

    JavaScrit

---
+++ product.js
@@ -0,0 +1,6 @@
+function mywork(a, b){
+    var product = a * b;
+  return product;
+}
+var result = mywork (5,5);
+console.log(result);
---
+++ sum.js
@@ -0,0 +1,6 @@
+function mywork(a, b){
+    var sum = a +b;
+    return sum;
+}
+var result = mywork (5,5);
+console.log(result);
